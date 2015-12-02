12a16acdadcd1bae5aa7579d0be7bd7b328476b0
js31g12 <js31g12@soton.ac.uk>
2015-12-02 01:44:09 +0000

    Working compressor function and .py for testing functions

--- spb.py
+++ spb.py
@@ -1,32 +1,71 @@
 import numpy as np
 
+def amp2dB(x):
+	y = 20*np.log10(x)
+	return y
+
+def dB2amp(x):
+	y = np.power(10,x/20)
+	return y
+
 def gain(x,gdB):
+	"""Applies decibel gain to a signal.
+	
+	ARGUMENTS
+	x : array
+		Input array.
+	gdB : float
+		Desired gain (in dB).
+	
+	RETURNS
+	y : numpy.ndarray
+		Array x with gain applied.
+				
+	"""
 	y = np.multiply(np.power(10,gdB/20.0),x)
 	return y
 	
-def envelope_detection(x,fs,tauA,tauR): # Branching smooth envelope detection method
-	alphaA = np.exp(-1/(tauA*fs)) # Convert tauA and tauR (attack and release time constants) to time
+def compressor(x,fs,tauA,tauR,T,CR,KW,MG):
+	"""Applies dynamic compression to the signal.
+	
+	ARGUMENTS
+	x : array
+		Input array.
+	T : float
+		Threshold of compressor (in dB).
+	tauA : float
+		Attack time (in seconds).
+	tauR : float
+		Release time (in seconds).
+	
+	RETURNS
+	y : numpy.ndarray
+		Compresse.
+				
+	"""
+	# Extract envelope
+	alphaA = np.exp(-1/(tauA*fs)) # Convert tauA and tauR (attack and release times) for use in algorithm
 	alphaR = np.exp(-1/(tauR*fs))
-	y = abs(x)
+	x_abs = abs(x)
 	c = 0 # Dummy variable for incoming signal
-	for i in range(0,len(y)):
-		if y[i] > c: # When next sample is increasing
-			c = alphaA*c+(1-alphaA)*y[i] # Attack
+	x_env = np.zeros(len(x))
+	for i in range(0,len(x)):
+		if x_abs[i] > c: # When next sample is increasing
+			c = alphaA*c+(1-alphaA)*x_abs[i] # Attack
 		else: # When next sample is decreasing
-			c = alphaR*c+(1-alphaR)*y[i] # Release
-		y[i] = c # Assig new value to y
-	return y
-
-def compressor(x,T,CR,KW,MG):
+			c = alphaR*c+(1-alphaR)*x_abs[i] # Release
+		x_env[i] = c # Assig new value to y
 	# Compute gain
-	xdB = 20*np.log10(x)
+	xdB_env = amp2dB(x_env)
+	xdB = amp2dB(x)
 	ydB = np.zeros(len(x))
-	for i in range(0,len(xdB)):
-		if 2*(xdB[i]-T) < -KW: # Below threshold
+	for i in range(0,len(x)):
+		if 2*(xdB_env[i]-T) < -KW: # Below threshold
 			ydB[i] = xdB[i]
-		elif 2*(abs(xdB[i]-T)) <= KW: # Within knee range
+		elif 2*(abs(xdB_env[i]-T)) <= KW: # Within knee range
 			ydB[i] = xdB[i]+(1/CR-1)*np.power((xdB[i]-T+KW/2),2)/(2*KW)
-		elif 2*(x[i]-T) > KW: # Over threshold
+		elif 2*(xdB_env[i]-T) > KW: # Over threshold
 			ydB[i] = T+(xdB[i]-T)/CR
-	y = np.power(10,ydB/20)
+	ydB = ydB+MG
+	y = dB2amp(ydB)
 	return y
---
+++ test_spb.py
@@ -0,0 +1,76 @@
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+import numpy as np
+import timeit
+
+from spb import *
+
+def pcm2float(x,dtype='float64'):
+	"""Convert a PCM signal to floating point data with a range from -1 to 1.
+	
+	ARGUMENTS
+	x : array
+		Input array of integral type.
+	dtype : data type, optional
+		Desired (floating point) data type.
+	
+	RETURNS
+	numpy.ndarray
+		Normalised floating point data.
+				
+	"""
+	x = np.asarray(x) # Convert to numpy array
+	if x.dtype.kind not in 'iu': # Check that x is a signed (i) or unsinged (u) integer
+		raise TypeError("'x' must be an array of integers")
+	dtype = np.dtype(dtype) # Convert string to data type object
+	if dtype.kind != 'f': # Check that dtype is a floating point type
+		raise TypeError("'dtype' must be a floating point type")
+	i = np.iinfo(x.dtype) # Find machine limits for data type
+	abs_max = 2**(i.bits-1) # Find absolute max limits
+	offset = i.min + abs_max # Find offset
+	return (x.astype(dtype)-offset)/abs_max # Convert signal to floating point type
+
+""" Load sample file """
+
+fs,x = wavfile.read('runninga_20s.wav') # Open file
+chunk = 128 # Buffer size
+x = x[0:chunk-1] # Limit to buffer size
+x = pcm2float(x) # Convert to float
+t = np.arange(0,len(x))/fs # Generate time vector
+
+""" Test gain """
+
+fig1 = plt.figure(1)
+
+G = -6
+
+y = gain(x,G)
+
+plt.plot(t,x)
+plt.plot(t,y,'r')
+plt.title('Gain of '+str(G)+' dB applied')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+
+print('Gain = ' + str(np.average(timeit.repeat("gain(x,G)","from __main__ import gain,x,G",number=1,repeat=100))*1000) + ' ms')
+
+""" Test compressor """
+
+fig2 = plt.figure(2)
+
+tauA = 0.0001
+tauR = 0.05
+T = -10
+CR = 2
+KW = 10
+MG = 0
+
+y = compressor(x,fs,tauA,tauR,T,CR,KW,MG)
+
+plt.plot(t,x)
+plt.plot(t,y,'r')
+plt.title('Envelope detection applied')
+plt.xlabel('Time (s)')
+plt.ylabel('Amplitude')
+
+print('Compressor = ' + str(np.average(timeit.repeat("compressor(x,fs,tauA,tauR,T,CR,KW,MG)","from __main__ import compressor,x,fs,tauA,tauR,T,CR,KW,MG",number=1,repeat=100))*1000) + ' ms')
