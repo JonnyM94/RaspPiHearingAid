976c06074f117f5c56094eac809600654eab491b
cps1g12 <charles.saunders94@gmail.com>
2015-12-03 17:34:48 +0000

    Added simulated feedback function
    Added function to do the feedback simulation.

--- hearing_aid.py
+++ hearing_aid.py
@@ -1,5 +1,5 @@
 """ FUTURE IMPORTS """
-
+import platform
 from __future__ import print_function, division, absolute_import, unicode_literals
 
 
@@ -9,23 +9,23 @@ from __future__ import print_function, division, absolute_import, unicode_litera
 # Check if debug mode should be enabled
 ui_gui = raw_input('Would you like to enable the GUI? [y/n] ')
 if ui_gui == 'y':
-	gui = True
+        gui = True
 else:
-	gui = False
+        gui = False
 
 ui_debug = raw_input('Would you like to enable debugging? [y/n] ')
 if ui_debug == 'y':
-	debug = True
+        debug = True
 else:
-	debug = False
+        debug = False
 
 
 
 """ DEBUGGING FUNCTIONS """
 
 def print_debug(x):
-	if debug == True:
-		print('# ',x)
+        if debug == True:
+                print('# ',x)
 
 
 
@@ -33,23 +33,23 @@ def print_debug(x):
 
 """
 
-os 			# For checking files
-ConfigParser	# For reading and writing .ini files
-alsaaudio		# For communicating with ALSA
-threading		# For multi-threading
-numpy			# For scientific computing
-spb			# Signal processing blocks for hearing aid
-Tkinter		# For building GUI
-matplotlib		# For plotting graphs
-	
+os                      # For checking files
+ConfigParser    # For reading and writing .ini files
+alsaaudio               # For communicating with ALSA
+threading               # For multi-threading
+numpy                   # For scientific computing
+spb                     # Signal processing blocks for hearing aid
+Tkinter         # For building GUI
+matplotlib              # For plotting graphs
+        
 """
 
 print_debug('Importing modules...')
 
-import os, ConfigParser, alsaaudio, threading, numpy as np, spb
+import os, ConfigParser, threading, numpy as np, spb
 
 if gui == True: # Only import is GUI is required
-	import Tkinter as tk, matplotlib.pyplot as plt
+        import Tkinter as tk, matplotlib.pyplot as plt
 
 
 
@@ -59,12 +59,12 @@ print_debug('Reading config files...')
 
 cfg = ConfigParser.ConfigParser()
 if os.path.isfile("config.ini"):
-	cfg.read('config.ini') # Load 'config.ini', if it exists
+        cfg.read('config.ini') # Load 'config.ini', if it exists
 elif os.path.isfile("defaults.ini"):
-	cfg.read('defaults.ini') # Load 'defaults.ini'
+        cfg.read('defaults.ini') # Load 'defaults.ini'
 else:
-	print("No .ini files exist! Exiting...")
-	exit()
+        print("No .ini files exist! Exiting...")
+        exit()
 
 print_debug('Assigning global variables...')
 
@@ -85,7 +85,9 @@ COMPRESSOR_CR = cfg.getfloat('compressor','compression_ratio')
 COMPRESSOR_MG = cfg.getfloat('compressor','makeup_gain')
 COMPRESSOR_KW = cfg.getfloat('compressor','knee_width')
 
-
+#Feedback simulation
+FEEDBACK_DELAY = cfg.getfloat('feedback_simulation','sample_delay')
+FEEDBACK_GAIN = cfg.getfloat('feedback_simulation','feedback_gain')
 
 """ SET AUDIO PARAMETERS """
 
@@ -100,19 +102,23 @@ fs = cfg.getint('i/o','fs')
 
 """ SET INPUT AND OUTPUT """
 
-print_debug('Reticulating splines...')
+if platform.system()!='Windows':  #Don't load ALSA stuff when testing on windows
+        import alsaaudio
 
 inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NORMAL,card)
-inp.setchannels(channels)
-inp.setrate(fs)
-inp.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
-inp.setperiodsize(chunk)
+        print_debug('Reticulating splines...')
 
-out = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK,alsaaudio.PCM_NORMAL,card)
-out.setchannels(channels)
-out.setrate(fs)
-out.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
-out.setperiodsize(chunk)
+        inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NORMAL,card)
+        inp.setchannels(channels)
+        inp.setrate(fs)
+        inp.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
+        inp.setperiodsize(chunk)
+
+        out = alsaaudio.PCM(alsaaudio.PCM_PLAYBACK,alsaaudio.PCM_NORMAL,card)
+        out.setchannels(channels)
+        out.setrate(fs)
+        out.setformat(alsaaudio.PCM_FORMAT_FLOAT_LE)
+        out.setperiodsize(chunk)
 
 
 
@@ -120,21 +126,32 @@ out.setperiodsize(chunk)
 
 silence = chr(0)*channels*chunk*2
 
+
 def playAudio():
-	while not finish:
-		if BYPASS['AUDIO'] == False:
-			l,data = inp.read() # Find length and extract data from stream
-			if l == chunk: # Check that the length
-				data_proc = np.fromstring(data,dtype="float32")
-				if BYPASS['GAIN'] == False:
-					data_proc = spb.gain(data_proc,GAIN)
-				data_out = np.array(data_proc,dtype="float32")
-				out.write(data_out)
-			else:
-				out.write(silence)
-	else:
-		pass
-	time.sleep(0.001)
+        output_feedback = np.zeros(chunk*2)
+        
+        while not finish:
+                if BYPASS['AUDIO'] == False:
+                        l,data = inp.read() # Find length and extract data from stream
+                        if l == chunk: # Check that the length
+                                data_proc = np.fromstring(data,dtype="float32")
+
+                                if BYPASS['FEEDBACK'] == False: #Add last output to current input
+                                        data_proc = spb.add_feedback(data_proc,output_feedback,FEEDBACK_GAIN)
+
+                                if BYPASS['GAIN'] == False:
+                                        data_proc = spb.gain(data_proc,GAIN)
+
+                                if BYPASS['FEEDBACK'] == False:  #Store the output buffer for feedback
+                                        output_feedback = data_proc
+                                        
+                                data_out = np.array(data_proc,dtype="float32")
+                                out.write(data_out)
+                        else:
+                                out.write(silence)
+        else:
+                pass
+        time.sleep(0.001)
 
 
 
@@ -142,48 +159,59 @@ def playAudio():
 
 if gui == False:
 
-	BYPASS = {
-			'AUDIO': False,
-			'GAIN':  False,
-			}
-			
+        BYPASS = {
+                        'AUDIO': False,
+                        'GAIN':  False,
+                        'FEEDBACK': False,
+                        }
+                        
 elif gui == True:
-	
-	BYPASS = {
-			'AUDIO': True,
-			'GAIN':  True,
-			}
-	
-	global btn_toggle_audio, btn_toggle_gain
-	
-	def toggle_audio():
-		global BYPASS
-		if BYPASS['AUDIO'] == True:
-			BYPASS['AUDIO'] = False
-			btn_toggle_audio.config(text='Stop audio')
-		elif BYPASS['AUDIO'] == False:
-			BYPASS['AUDIO'] = True
-			btn_toggle_audio.config(text='Start audio')
-	
-	def toggle_gain():
-		global BYPASS
-		if BYPASS['GAIN']  == True:
-			BYPASS['GAIN']  = False
-			btn_toggle_gain.config(text='Gain ON')
-		else:
-			BYPASS['GAIN']  = True
-			btn_toggle_gain.config(text='Gain OFF')
+        
+        BYPASS = {
+                        'AUDIO': True,
+                        'GAIN':  True,
+                        'FEEDBACK': True,
+                        }
+        
+        global btn_toggle_audio, btn_toggle_gain, btn_toggle_feedback
+        
+        def toggle_audio():
+                global BYPASS
+                if BYPASS['AUDIO'] == True:
+                        BYPASS['AUDIO'] = False
+                        btn_toggle_audio.config(text='Stop audio')
+                elif BYPASS['AUDIO'] == False:
+                        BYPASS['AUDIO'] = True
+                        btn_toggle_audio.config(text='Start audio')
+        
+        def toggle_gain():
+                global BYPASS
+                if BYPASS['GAIN']  == True:
+                        BYPASS['GAIN']  = False
+                        btn_toggle_gain.config(text='Gain ON')
+                else:
+                        BYPASS['GAIN']  = True
+                        btn_toggle_gain.config(text='Gain OFF')
+
+        def toggle_feedback():
+                global BYPASS
+                if BYPASS['FEEDBACK']  == True:
+                        BYPASS['FEEDBACK']  = False
+                        btn_toggle_feedback.config(text='Feedback ON')
+                else:
+                        BYPASS['FEEDBACK']  = True
+                        btn_toggle_feedback.config(text='Feedback OFF')
 
 
 
 """ GUI INTERFACING """
 
 if gui == True:
-	
-	# Gain
-	def get_gain(val):
-	    global GAIN
-	    GAIN = float(val)   
+        
+        # Gain
+        def get_gain(val):
+            global GAIN
+            GAIN = float(val)   
 
 
 
@@ -192,26 +220,29 @@ if gui == True:
 print_debug('Generating GUI...')
 
 def GUI():
-	
-	global finish
-	global BYPASS
-	global btn_toggle_audio, btn_toggle_gain
-	
-	root=tk.Tk()
-	
-	btn_toggle_audio = tk.Button(root, text='Start audio', command=toggle_audio)
-	btn_toggle_audio.pack()
+        
+        global finish
+        global BYPASS
+        global btn_toggle_audio, btn_toggle_gain, btn_toggle_feedback
+        
+        root=tk.Tk()
+        
+        btn_toggle_audio = tk.Button(root, text='Start audio', command=toggle_audio)
+        btn_toggle_audio.pack()
 
-	slider_gain = tk.Scale(root,from_=-20,to=6,resolution=0.1,label='GAIN',orient='horizontal',command=get_gain,length=120)
-	slider_gain.set(GAIN)
-	slider_gain.pack(fill='x')
-	
-	btn_toggle_gain = tk.Button(root, text="Gain OFF", command=toggle_gain)
-	btn_toggle_gain.pack()
-	
-	root.mainloop()
-	    
-	finish = True
+        slider_gain = tk.Scale(root,from_=-20,to=6,resolution=0.1,label='GAIN',orient='horizontal',command=get_gain,length=120)
+        slider_gain.set(GAIN)
+        slider_gain.pack(fill='x')
+        
+        btn_toggle_gain = tk.Button(root, text="Gain OFF", command=toggle_gain)
+        btn_toggle_gain.pack()
+
+        btn_toggle_feedback = tk.Button(root, text="Feedback OFF", command=toggle_feedback)
+        btn_toggle_feedback.pack()
+        
+        root.mainloop()
+            
+        finish = True
 
 
 """ THREADING """
--- spb.py
+++ spb.py
@@ -3,3 +3,7 @@ import numpy as np
 def gain(x,gdB):
 	y = np.multiply(np.power(10,gdB/20.0),x)
 	return y
+
+def add_feedback(input_signal,previous_output,gain):
+    return np.add(input_signal,np.multiply(previous_output,gain))
+	
