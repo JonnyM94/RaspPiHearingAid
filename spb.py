a22669cbeeb5b1b1a5c53e184faf86b7667ebedf
js31g12 <js31g12@soton.ac.uk>
2015-12-04 14:02:49 +0000

    Imported only the necessary modules

--- spb.py
+++ spb.py
@@ -1,9 +1,9 @@
-import numpy as np
+from numpy import multiply, power, add
 
 def gain(x,gdB):
-	y = np.multiply(np.power(10,gdB/20.0),x)
+	y = multiply(power(10,gdB/20.0),x)
 	return y
 
 def add_feedback(input_signal,previous_output,gain):
-    return np.add(input_signal,np.multiply(previous_output,gain))
+    return add(input_signal,multiply(previous_output,gain))
 	
